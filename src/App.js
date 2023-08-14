17cbb5a6ccbec4b4dedb8d44ed579617932e2de6
nehoraiharush <nehorai883@gmail.com>
2023-08-14 23:00:25 +0300

    more changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Component from './Component';
 import H1 from './H1';
+import H2 from './H2';
 
 export const APPNAME = '/my-app'
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path={`${APPNAME}/`} element={<Component />} />
         <Route path={`${APPNAME}/h1`} element={<H1 />} />
+        <Route path={`${APPNAME}/h2`} element={<H2 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/H2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const H2 = () => {
+    return (
+        <div style={{ height: '100vh', width: '100%', display: 'grid', placeItems: 'center' }}>
+            <h1 style={{ backgroundColor: '#3fc', padding: '50px', borderRadius: '10px' }}>
+                ISI
+            </h1>
+        </div>
+    )
+}
+
+export default H2
