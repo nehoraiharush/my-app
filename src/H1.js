412939bbe63cdc1caaf86f94e397ae48240cfc43
nehoraiharush <nehorai883@gmail.com>
2023-08-14 21:01:09 +0300

    Update H1.js

--- src/H1.js
+++ src/H1.js
@@ -7,8 +7,8 @@ const H1 = () => {
     const text = state?.text;
 
     return (
-        <div style={{ height: '100vh', width: '100%' }}>
-            <h1 style={{ backgroundColor: '#3fc' }}>
+        <div style={{ height: '100vh', width: '100%', display: 'grid', placeItems: 'center' }}>
+            <h1 style={{ backgroundColor: '#3fc', padding: '50px', borderRadius: '10px' }}>
                 {text}
             </h1>
         </div>
